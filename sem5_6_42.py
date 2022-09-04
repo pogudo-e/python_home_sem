e09404182d800fca0fdb9ff8ec082b41791a38b5
Evgeny Pogudo <sensesbit@gmail.com>
2022-09-04 11:17:48 +0300

    42 rle algoritm

---
+++ sem5_6_42.py
@@ -0,0 +1,42 @@
+# Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных.
+# входные и выходные данные хранятся в отдельных текстовых файлах
+
+
+
+with open('sem5_6_42_d.txt', 'r') as data:
+    my_text = data.read()
+
+def encode_rle(ss):
+    str_code = ''
+    prev_char = ''
+    count = 1
+    for char in ss:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    return str_code
+
+            
+str_code = encode_rle(my_text)
+print(f'Encode: {str_code}')
+
+with open('sem5_6_42_e.txt', 'r') as data:
+    my_text2 = data.read()
+
+def decoding_rle(ss:str):
+    count = ''
+    str_decode = ''
+    for char in ss:
+        if char.isdigit():
+            count += char 
+        else:
+            str_decode += char * int(count)
+            count = ''
+    return str_decode
+
+str_decode = decoding_rle(my_text2)
+print(f'Decode: {str_decode}')
