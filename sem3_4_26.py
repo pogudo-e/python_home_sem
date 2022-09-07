57bd33696bdaa1087c6121571820759222bc33b7
Evgeny Pogudo <sensesbit@gmail.com>
2022-09-07 16:19:13 +0300

    oprimise 26 hw

--- sem3_4_26.py
+++ sem3_4_26.py
@@ -6,24 +6,40 @@
 num = int(input('Введите число: '))
 
 
+# def fibonacci(n):
+#     a, b = 1, 1
+#     for i in range(n):
+#         yield a
+#         a, b = b, a + b
+
+# def rever(lis):
+#     data2 = []
+#     count = 1
+#     for n in lis:
+#         if count % 2 == 0:
+#             data2.append(-n)
+#         else:
+#             data2.append(n)
+#         count += 1
+#     data2.reverse()
+#     data2.append(0)
+#     return data2
+
+# data = list(fibonacci(num))
+# print(rever(data) + data)
+
 def fibonacci(n):
+    fibo_nums = []
     a, b = 1, 1
     for i in range(n):
-        yield a
+        fibo_nums.append(a)
         a, b = b, a + b
+    a, b = 0, 1
+    for i in range (n+1):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
 
-def rever(lis):
-    data2 = []
-    count = 1
-    for n in lis:
-        if count % 2 == 0:
-            data2.append(-n)
-        else:
-            data2.append(n)
-        count += 1
-    data2.reverse()
-    data2.append(0)
-    return data2
-
-data = list(fibonacci(num))
-print(rever(data) + data)
+fibo_nums = fibonacci(num)
+print(fibonacci(num))
+print(fibo_nums.index(0))
