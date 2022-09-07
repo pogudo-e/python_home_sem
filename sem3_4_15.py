71ec41e01a12e00986772c08e2f0df6b901708bb
Evgeny Pogudo <sensesbit@gmail.com>
2022-09-07 16:13:03 +0300

    oprimise 15 hw

--- sem3_4_15.py
+++ sem3_4_15.py
@@ -2,16 +2,28 @@
 #Пример:
 #- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-n = int(input('Введите вещественное число: '))
+# n = int(input('Введите вещественное число: '))
 
-def f (n):
-    ans = 1
-    i = 1
-    arr = []
-    while n+1 > i:
-        ans = ans * i
-        arr.append(ans)
-        i += 1
-    return arr
+# def f (n):
+#     ans = 1
+#     i = 1
+#     arr = []
+#     while n+1 > i:
+#         ans = ans * i
+#         arr.append(ans)
+#         i += 1
+#     return arr
 
-print(f(n))
+# print(f(n))
+
+
+from itertools import accumulate
+import operator
+
+n = int(input('Введите число: '))
+
+
+def get_prods(n):
+    return list(accumulate([x for x in range(1, n + 1)], operator.mul))
+
+print(get_prods(n))
